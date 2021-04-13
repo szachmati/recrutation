c6bded51e94088e2a576cb78666bbd83fa93265a
szachMati <donek8@gmail.com>
2021-04-13 23:01:52 +0200

    file segregator

--- segregation/src/main/java/pl/files/FileSegregator.java
+++ segregation/src/main/java/pl/files/FileSegregator.java
@@ -17,25 +17,35 @@ public class FileSegregator {
      * if file extension is jar and creation date of file mod2 == 0 then move file to DEV folder
      * if file extension is jar and creation date of file mod2 != 0 then move file to TEST folder
      * if file extension is xml move file to dev folder
-     * */
+     */
     public static void segregateFiles(BasicFileAttributes fileAttributes, Path path) throws IOException {
         FileTime fileTime = fileAttributes.creationTime();
         String fileExtension = FilenameUtils.getExtension(path.toString());
+        Path newPath = null;
 
-        switch(fileExtension) {
+        switch (fileExtension) {
             case JAR:
                 if (fileTime.toMillis() % 2 == 0) {
-                    Files.move(path, Paths.get(buildDirectoryPath(DEV + SLASH + path.getFileName().toString())));
+                    newPath = Paths.get(buildDirectoryPath(DEV + SLASH + path.getFileName().toString()));
+                    moveFile(path, newPath);
                 } else {
-                    Files.move(path, Paths.get(buildDirectoryPath(TEST + SLASH + path.getFileName().toString())));
+                    newPath = Paths.get(buildDirectoryPath(TEST + SLASH + path.getFileName().toString()));
+                    moveFile(path, newPath);
                 }
+                break;
             case XML:
-                Files.move(path, Paths.get(buildDirectoryPath(DEV + SLASH + path.getFileName().toString())));
+                newPath = Paths.get(buildDirectoryPath(DEV + SLASH + path.getFileName().toString()));
+                moveFile(path, newPath);
+                break;
             default:
                 break;
         }
+    }
 
-
+    private static void moveFile(Path oldPath, Path newPath) throws IOException {
+        if (!Files.exists(newPath)) {
+            Files.move(oldPath, newPath);
+        }
     }
 
 }
